9403ed0abaea743770d2804e268c37a6bb5c852f
Andres Villamil <andresvillamil991@gmail.com>
2026-05-03 12:17:41 -0500

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Sistema Super Roca</h1>
+      <p>App de producción en funcionamiento</p>
+    </div>
+  );
+}
